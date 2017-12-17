e90fe0dce3c94f3586706f3b75fda926ffd3c139
Douglas Amorim dos Santos <30800358+DouglasAmorim@users.noreply.github.com>
2017-12-17 13:52:01 -0200

    UART_TX pronta // faltando RX e conversão ssd

--- conv_paralelo_serial.vhd
+++ conv_paralelo_serial.vhd
@@ -4,38 +4,55 @@ USE ieee.std_logic_1164.ALL;
 ENTITY conv_paralelo_serial IS
     GENERIC (N : INTEGER := 10);
     PORT (
-        clk, baudrate, parity, rst : IN STD_LOGIC;
-        par_in : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        ser_out, sclk_test : OUT STD_LOGIC
+        clk, baudrate, rst : IN STD_LOGIC;
+		  parity: IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        par_in : IN STD_LOGIC_VECTOR(N DOWNTO 0);
+        ser_out: OUT STD_LOGIC
     );
 END ENTITY;
 
 ARCHITECTURE conv_paralelo_serial OF conv_paralelo_serial IS
     SIGNAL sclk : STD_LOGIC;
+	 SIGNAL rst_t: std_logic;
     SIGNAL internal : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-    COMPONENT altera_pll IS
-        PORT (
-            areset : IN STD_LOGIC := '0';
-            inclk0 : IN STD_LOGIC := '0';
-            c0 : OUT STD_LOGIC;
-            locked : OUT STD_LOGIC
-        );
-    END COMPONENT;
+	 
+--    COMPONENT altera_pll IS
+--        PORT (
+--            areset : IN STD_LOGIC := '0';
+--            inclk0 : IN STD_LOGIC := '0';
+--            c0 : OUT STD_LOGIC;
+--            locked : OUT STD_LOGIC
+--        );
+--    END COMPONENT;
+	 
 BEGIN
-    pll_circuit : altera_pll
-    PORT MAP('0', clk, sclk, OPEN);
-    sclk_test <= sclk;
-    PROCESS (sclk)
-    VARIABLE count : INTEGER RANGE 0 TO N;
+--    pll_circuit : altera_pll
+--	 
+--    PORT MAP('0', clk, sclk, OPEN);	
+	rst_t <= not rst;
+	 
+    PROCESS (clk, rst_t)
+    VARIABLE count: INTEGER RANGE 0 TO N+2;
+	  VARIABLE c_data : INTEGER RANGE 0 TO N;
     BEGIN
-        IF (sclk'EVENT AND sclk = '1') THEN
-            count := count + 1;
-            IF (count = N - 1) THEN
-                internal <= din;
-            ELSIF (count = N) THEN
-                count := 0;
-            END IF;
-            dout <= internal(count);
+		  IF(rst_t = '1') THEN
+				count := 0;
+				c_data := 0;
+				ser_out <= '0';
+		  elsIF (clk'EVENT AND clk = '1') THEN
+				if(count = N+2) then
+					count := 0;
+					c_data := 0;
+				elsif(count = 0) then
+					count := count + 1;
+					ser_out <= baudrate;
+				elsif(count = 1) then
+					ser_out <= parity(0);
+					count := count + 1;
+				else
+            ser_out <= par_in(c_data);
+				c_data := c_data + 1;
+				end if;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
--- stsass.vhd
+++ stsass.vhd
@@ -1,94 +1,49 @@
-library ieee;
-use ieee.std_logic_1164.all;
-library altera;
-use altera.altera_standard_functions.all;
-entity stsass is
-    port (
-        clk50MHz, sw_rst, ser_in, sw_parit_rx, sw_parit_tx : in std_logic;
-        sw_baud_rx, sw_baud_tx : in std_logic_vector(1 downto 0);
-        sw_nrb : in std_logic_vector(2 downto 0);
-        data_in : in std_logic_vector(7 downto 0);
-        ser_out, ser_by, rx_error : out std_logic;
-        hex_txa, hex_txb, hex_rxa, hex_rxb : out std_logic_vector(6 downto 0);
-        data_out : out std_logic_vector(7 downto 0);
-        let_tx, led_rx : out std_logic_vector(7 downto 0)
-    );
-end stsass;
-architecture rtl of stsass is
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+LIBRARY altera;
+USE altera.altera_standard_functions.ALL;
 
-    signal baud_rate_seri: std_LOGIC;
-	 signal baud_rate_deseri: std_LOGIC;
-    signal parity_seri: std_logic;
-	 signal parity_deseri: std_logic;
-	 signal par_out_deseri: std_LOGIC_VECTOR(n-1 downto 0);
-	 
-    component conv_paralelo_serial is
-        generic (N : integer := 10);
-        port (
-            clk, baudrate, parity, rst : in STD_LOGIC;
-            par_in : in STD_LOGIC_VECTOR(N - 1 downto 0);
-            ser_out, sclk_test : out STD_LOGIC
-        );
-    end component;
-    component conv_serial_paralelo_rx is
-        generic (bits : positive := 14);
-        port (
-            clk, rst, baudrate, ser_in : in std_logic;
-            parity : out std_logic;
-            par_out : out std_logic_vector(n - 1 downto 0)
-        );
-    end component;
+ENTITY stsass IS
+	PORT (
+		clk50MHz, sw_rst, ser_in, sw_parity_rx, sw_parity_tx : IN std_logic;
+		sw_baud_rx, sw_baud_tx : IN std_logic_vector(1 DOWNTO 0);
+		sw_nrb : IN std_logic_vector(2 DOWNTO 0);
+		data_in : IN std_logic_vector(7 DOWNTO 0);
+		ser_out, ser_by, rx_error : OUT std_logic;
+		hex_txa, hex_txb, hex_rxa, hex_rxb : OUT std_logic_vector(6 DOWNTO 0);
+		data_out : OUT std_logic_vector(7 DOWNTO 0);
+		let_tx, led_rx : OUT std_logic_vector(7 DOWNTO 0)
+	);
+END stsass;
+ARCHITECTURE rtl OF stsass IS
+	signal dados_tx: std_logic_vector(10 downto 0);
+	COMPONENT uart_tx
+		GENERIC
+		(
+			NUM_DADOS : natural := 10
+		);
+		port(
+			sw_parit_TX, clk50mhz, rst: in std_logic;
+			data_in: in std_logic_vector(NUM_DADOS downto 0);
+			sw_baund_tx: in std_logic_vector(1 downto 0);
+			ser_out: out std_logic
+		);
+	END COMPONENT;
+	
+	COMPONENT uart_rx
+		
+	END COMPONENT;
+	
+	COMPONENT entrada_dados
+		
+	END COMPONENT;
+	
+	COMPONENT saida_dados
+		
+	END COMPONENT;
+	
+BEGIN
 
-    component baud_rate is
-        port (
-            clk_in : in STD_LOGIC;
-            rst : in std_LOGIC;
-            sw_baud : in std_logic_vector(1 downto 0);
-            baudrate : out STD_LOGIC
-        );
-    end component;
-
-    component gerador_paridade is
-        generic (
-            NUM_DADOS : natural := 9
-        );
-        port (
-            DATA_IN : in STD_LOGIC_VECTOR (NUM_DADOS downto 0);
-            SW_PARITY_TX : in STD_LOGIC;
-            PARITY : out STD_LOGIC_vector(0 downto 0)
-        );
-    end component;
-
-    component detector_paridade is
-        generic (bits : positive := 10);
-        port (
-            dados_in : in std_logic_vector(bits - 1 downto 0);
-            parity, im_par : in std_logic;
-            output : out std_logic
-        );
-    end component;
-
-    component uart_rx is
-        port (
-            sw_parit_rX, clk50mhz, rst : in std_logic;
-            ser_in : in std_logic;
-            sw_baud_rx : in std_logic_vector(1 downto 0);
-            data_out : out std_logic_vector(10 downto 0);
-            error : out std_logic
-        );
-    end component;
-	 
-    component uart_tx is
-        port (
-            sw_parit_TX, clk50mhz, rst : in std_logic;
-            data_in : in std_logic_vector(10 downto 0);
-            sw_baund_tx : in std_logic_vector(1 downto 0);
-            ser_out : out std_logic
-        );
-    end component;
-begin
-    serializador: conv_paralelo_serial
-	 port map(clk50MHz, baud_rate_seri, parity_seri, sw_rst, data_in, ser_out, open);
-	 deserializador: conv_paralelo_serial
-	 port map(clk50MHz, sw_rst, baud_rate_deseri, datA_IN, parity_deseri, par_out_deseri);
-end rtl;
+inst1: uart_tx
+	port map(sw_parity_tx ,clk50MHz, sw_rst, dados_tx, sw_baud_tx,ser_out);
+END rtl;
